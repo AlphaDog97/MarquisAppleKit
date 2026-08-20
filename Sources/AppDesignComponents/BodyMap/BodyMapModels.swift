570826fcc543a9ea7c89d85f2abe7b1c0da2a036
AlphaDog <103195466+AlphaDog97@users.noreply.github.com>
2026-08-20 22:30:39 +0800

    Implement BodyMap public component models

--- Sources/AppDesignComponents/BodyMap/BodyMapModels.swift
+++ Sources/AppDesignComponents/BodyMap/BodyMapModels.swift
@@ -1,5 +1,10 @@
 import SwiftUI
 
+public enum BodyMapModel: Sendable {
+    case male
+    case female
+}
+
 public struct BodyMapRegionID: Hashable, Sendable, RawRepresentable {
     public let rawValue: String
 
@@ -15,7 +20,7 @@ public struct BodyMapRegionStyle: Identifiable {
     public let glow: BodyMapGlowStyle
     public let shadow: BodyMapShadowStyle
     public let isSelected: Bool
-    public let animationFactor: Double
+    public let revealFactor: Double
 
     public init(
         id: BodyMapRegionID,
@@ -24,7 +29,7 @@ public struct BodyMapRegionStyle: Identifiable {
         glow: BodyMapGlowStyle = .init(),
         shadow: BodyMapShadowStyle = .init(),
         isSelected: Bool = false,
-        animationFactor: Double = 1
+        revealFactor: Double = 1
     ) {
         self.id = id
         self.color = color
@@ -32,42 +37,72 @@ public struct BodyMapRegionStyle: Identifiable {
         self.glow = glow
         self.shadow = shadow
         self.isSelected = isSelected
-        self.animationFactor = animationFactor
+        self.revealFactor = revealFactor
     }
 }
 
-public struct BodyMapGlowStyle {
+public struct BodyMapGlowStyle: Sendable {
+    public let color: Color
     public let opacity: Double
     public let radius: CGFloat
+    public let energy: Double
+    public let falloff: Double
 
-    public init(opacity: Double = 0, radius: CGFloat = 0) {
+    public init(
+        color: Color = .clear,
+        opacity: Double = 0,
+        radius: CGFloat = 0,
+        energy: Double = 0,
+        falloff: Double = 1
+    ) {
+        self.color = color
         self.opacity = opacity
         self.radius = radius
+        self.energy = energy
+        self.falloff = falloff
     }
 }
 
-public struct BodyMapShadowStyle {
+public struct BodyMapShadowStyle: Sendable {
+    public let color: Color
     public let opacity: Double
     public let radius: CGFloat
+    public let energy: Double
 
-    public init(opacity: Double = 0, radius: CGFloat = 0) {
+    public init(
+        color: Color = .black,
+        opacity: Double = 0,
+        radius: CGFloat = 0,
+        energy: Double = 0
+    ) {
+        self.color = color
         self.opacity = opacity
         self.radius = radius
+        self.energy = energy
     }
 }
 
-public struct BodyMapAnimationConfiguration {
+public struct BodyMapAnimationConfiguration: Sendable {
     public let enabled: Bool
     public let revealDuration: Double
     public let transitionDuration: Double
+    public let selectionDuration: Double
+    public let regionDelay: Double
+    public let intensityDurationScaling: Double
 
     public init(
         enabled: Bool = true,
         revealDuration: Double = 0.6,
-        transitionDuration: Double = 0.3
+        transitionDuration: Double = 0.3,
+        selectionDuration: Double = 0.2,
+        regionDelay: Double = 0.05,
+        intensityDurationScaling: Double = 1
     ) {
         self.enabled = enabled
         self.revealDuration = revealDuration
         self.transitionDuration = transitionDuration
+        self.selectionDuration = selectionDuration
+        self.regionDelay = regionDelay
+        self.intensityDurationScaling = intensityDurationScaling
     }
 }
