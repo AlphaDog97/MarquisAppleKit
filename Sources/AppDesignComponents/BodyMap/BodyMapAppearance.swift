393dc7af01d6243cb12788398e129e5f710f0179
AlphaDog <103195466+AlphaDog97@users.noreply.github.com>
2026-08-21 13:20:20 +0800

    Add global BodyMap glow switch

--- Sources/AppDesignComponents/BodyMap/BodyMapAppearance.swift
+++ Sources/AppDesignComponents/BodyMap/BodyMapAppearance.swift
@@ -3,15 +3,18 @@ import SwiftUI
 public struct BodyMapAppearance {
     public let inactiveColor: Color
     public let backgroundColor: Color
+    public let glowEnabled: Bool
     public let regionStyles: [BodyMapRegionStyle]
 
     public init(
         inactiveColor: Color = .secondary,
         backgroundColor: Color = .clear,
+        glowEnabled: Bool = true,
         regionStyles: [BodyMapRegionStyle] = []
     ) {
         self.inactiveColor = inactiveColor
         self.backgroundColor = backgroundColor
+        self.glowEnabled = glowEnabled
         self.regionStyles = regionStyles
     }
 
@@ -23,6 +26,7 @@ public struct BodyMapAppearance {
         BodyMapAppearance(
             inactiveColor: inactiveColor,
             backgroundColor: backgroundColor,
+            glowEnabled: glowEnabled,
             regionStyles: regions
         )
     }
