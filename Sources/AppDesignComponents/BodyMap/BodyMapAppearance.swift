c03dcefb9410dc4c6429e32d5dbe5e82ac358e41
AlphaDog <103195466+AlphaDog97@users.noreply.github.com>
2026-08-21 16:14:49 +0800

    Expose BodyMap base opacity

--- Sources/AppDesignComponents/BodyMap/BodyMapAppearance.swift
+++ Sources/AppDesignComponents/BodyMap/BodyMapAppearance.swift
@@ -2,17 +2,20 @@ import SwiftUI
 
 public struct BodyMapAppearance {
     public let inactiveColor: Color
+    public let baseOpacity: Double
     public let backgroundColor: Color
     public let glowEnabled: Bool
     public let regionStyles: [BodyMapRegionStyle]
 
     public init(
         inactiveColor: Color = .secondary,
+        baseOpacity: Double = 0.10,
         backgroundColor: Color = .clear,
         glowEnabled: Bool = true,
         regionStyles: [BodyMapRegionStyle] = []
     ) {
         self.inactiveColor = inactiveColor
+        self.baseOpacity = baseOpacity
         self.backgroundColor = backgroundColor
         self.glowEnabled = glowEnabled
         self.regionStyles = regionStyles
@@ -25,6 +28,7 @@ public struct BodyMapAppearance {
     func withRegions(_ regions: [BodyMapRegionStyle]) -> BodyMapAppearance {
         BodyMapAppearance(
             inactiveColor: inactiveColor,
+            baseOpacity: baseOpacity,
             backgroundColor: backgroundColor,
             glowEnabled: glowEnabled,
             regionStyles: regions
