0a776819eff1e56b39576afcbb10cbc0bed5557a
AlphaDog <103195466+AlphaDog97@users.noreply.github.com>
2026-08-23 14:14:59 +0800

    Add inactive region color support for body map

--- Sources/AppDesignComponents/BodyMap/BodyMapAppearance.swift
+++ Sources/AppDesignComponents/BodyMap/BodyMapAppearance.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 public struct BodyMapAppearance {
     public let inactiveColor: Color
+    public let inactiveRegionColor: Color
     public let baseOpacity: Double
     public let backgroundColor: Color
     public let glowEnabled: Bool
@@ -9,12 +10,14 @@ public struct BodyMapAppearance {
 
     public init(
         inactiveColor: Color = .secondary,
+        inactiveRegionColor: Color = Color(.tertiaryLabel),
         baseOpacity: Double = 0.10,
         backgroundColor: Color = .clear,
         glowEnabled: Bool = true,
         regionStyles: [BodyMapRegionStyle] = []
     ) {
         self.inactiveColor = inactiveColor
+        self.inactiveRegionColor = inactiveRegionColor
         self.baseOpacity = baseOpacity
         self.backgroundColor = backgroundColor
         self.glowEnabled = glowEnabled
@@ -28,6 +31,7 @@ public struct BodyMapAppearance {
     func withRegions(_ regions: [BodyMapRegionStyle]) -> BodyMapAppearance {
         BodyMapAppearance(
             inactiveColor: inactiveColor,
+            inactiveRegionColor: inactiveRegionColor,
             baseOpacity: baseOpacity,
             backgroundColor: backgroundColor,
             glowEnabled: glowEnabled,
